3221940f1ed5bc2886d0f8ac5b4c22d4d0f811db
Alessandro Ranellucci <alessandro@pintle.it>
2022-02-17 20:19:13 +0100

    Faster implementation of installall

--- go.mod
+++ go.mod
@@ -57,7 +57,7 @@ require (
 	github.com/xanzy/ssh-agent v0.2.1 // indirect
 	go.bug.st/cleanup v1.0.0 // indirect
 	go.bug.st/downloader/v2 v2.1.1 // indirect
-	go.bug.st/relaxed-semver v0.0.0-20190922224835-391e10178d18 // indirect
+	go.bug.st/relaxed-semver v0.0.0-20190922224835-391e10178d18
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
--- internal/cli/installall.go
+++ internal/cli/installall.go
@@ -1,12 +1,21 @@
 package cli
 
 import (
+	"archive/zip"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
-	"github.com/alranel/arduino-testlib/internal/cliclient"
 	"github.com/alranel/arduino-testlib/internal/configuration"
+	"github.com/arduino/arduino-cli/arduino/utils"
 	"github.com/spf13/cobra"
+	semver "go.bug.st/relaxed-semver"
 )
 
 var installallCmd = &cobra.Command{
@@ -27,10 +36,124 @@ func runInstallall(cmd *cobra.Command, cliArguments []string) {
 		os.Exit(1)
 	}
 
-	cliclient.Init()
+	/*
+		// This implementation based on arduino-cli works but it's quite slow because
+		// arduino-cli is not optimized for bulk install (it reloads everything after
+		// the installation of each library).
+		cliclient.Init()
+		for _, lib := range cliclient.GetAllLibraries() {
+			// Use the unsanitized name to install the library
+			cliclient.InstallLibrary(lib, "")
+		}
+	*/
 
-	for _, lib := range cliclient.GetAllLibraries() {
-		// Use the unsanitized name to install the library
-		cliclient.InstallLibrary(lib, "")
+	// Read the library index
+	type library struct {
+		Name, Version, URL, ArchiveFileName string
 	}
+	libraries := make(map[string]library)
+	var jsonData struct{ Libraries []library }
+
+	jsonFile, err := os.Open(path.Join(configuration.CLIDataDir, "data/library_index.json"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to read library_index.json: %v\n", err)
+		os.Exit(1)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &jsonData)
+
+	// Find last version for each library
+	for _, lib := range jsonData.Libraries {
+		if l, ok := libraries[lib.Name]; ok {
+			if semver.ParseRelaxed(lib.Version).GreaterThan(semver.ParseRelaxed(l.Version)) {
+				libraries[lib.Name] = lib
+			}
+		} else {
+			libraries[lib.Name] = lib
+		}
+	}
+
+	// Download and unzip libraries
+	os.MkdirAll(path.Join(configuration.CLIDataDir, "downloads/libraries"), os.ModePerm)
+	os.MkdirAll(path.Join(configuration.CLIUserDir, "libraries"), os.ModePerm)
+	for libName, lib := range libraries {
+		fmt.Printf("Installing %s@%s\n", libName, lib.Version)
+
+		resp, _ := http.Get(lib.URL)
+		defer resp.Body.Close()
+		filename := path.Join(configuration.CLIDataDir, "downloads/libraries", lib.ArchiveFileName)
+		out, _ := os.Create(filename)
+		defer out.Close()
+		io.Copy(out, resp.Body)
+
+		libPath := path.Join(configuration.CLIUserDir, "libraries", utils.SanitizeName(libName))
+		_, err := unzip(filename, libPath)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func unzip(src string, destination string) ([]string, error) {
+	os.MkdirAll(destination, os.ModePerm)
+
+	var filenames []string
+	r, err := zip.OpenReader(src)
+
+	if err != nil {
+		return filenames, err
+	}
+
+	defer r.Close()
+
+	for _, f := range r.File {
+		// Remove root directory such as Foo-1.0.0 so that we put everything
+		// in the destination folder
+		f.Name = strings.SplitN(f.Name, "/", 2)[1]
+		if f.Name == "" {
+			continue
+		}
+		fpath := filepath.Join(destination, f.Name)
+
+		if !strings.HasPrefix(fpath, filepath.Clean(destination)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s is an illegal filepath (%s)", fpath, filepath.Clean(destination)+string(os.PathSeparator))
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath,
+			os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+			f.Mode())
+
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+
+	return filenames, nil
 }
